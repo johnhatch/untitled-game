07a66c686ff72fd6a67cb744105bb6cbedbf6dd8
John Hatch <billybombill@gmail.com>
2015-11-07 00:54:35 -0700

    Initial Commit

---
+++ items.js
@@ -0,0 +1,12 @@
+var items = {
+	sword: {
+		name: 'Sword',
+		html: '9876'
+	},
+	shield: {
+		name: 'Shield',
+		html: '9960',
+		dur: 75,
+		max_dur: 100
+	}
+}
---
+++ tiles.js
@@ -0,0 +1,20 @@
+var tiles = {
+	0: {
+		name: 'grass',
+		html: '9617'
+	},
+	1: {
+		name: 'tower',
+		html: '9814'
+	},
+	2: {
+		name: 'church',
+		html: '9962',
+		txt: 'An old church, looks to be abandoned.',
+		solid: true
+	},
+	3: {
+		name: 'player',
+		html: '9924'
+	}
+}
